243b526108a3183b5d9d97192b988e444c2b771d
Nicolas Ballas <ballas.n@gmail.com>
2014-07-03 09:05:19 -0400

    [FeatureExtraction]   * add densecuboids extraction code

---
+++ FeaturesExtraction/dense_cuboids/env.sh
@@ -0,0 +1,5 @@
+
+export OPENCV_DIST=/u/ballasn/project/opencv/dist
+
+export PATH="/u/ballasn/project/opencv/dist/bin:$PATH"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/u/ballasn/project/opencv/dist/lib"
